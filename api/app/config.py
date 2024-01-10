4816000c39df0175f7b7de26426d107641248c3b
jouhui <jouhui@cero.ai>
2024-01-10 19:38:01 -0300

    chore: convert bucket_model_path to en envvar

--- api/app/config.py
+++ api/app/config.py
@@ -2,11 +2,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    """Environment variables' settings for the API."""
+
     api_key: str
     api_host: str
     api_port: int
     google_application_credentials: str
     bucket_name: str
+    bucket_model_path: str
     dev_mode: bool
 
     model_config = SettingsConfigDict(env_file="api.env")
